2b919efd22e649fd3c42676117c6014bb229bc88
George Lesica <george@hobbes.(none)>
2010-10-22 00:11:12 -0500

    Changed doctype from html 5 to xhtml 1 transitional. Seeing if this resolves login bug.

--- main.php
+++ main.php
@@ -17,7 +17,8 @@
 // must be run from within DokuWiki
 if (!defined('DOKU_INC')) die();
 ?>
-<!doctype html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
