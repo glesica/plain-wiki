194eecd1f484214e1e11a5cf8c19789c3fc656f0
George Lesica <glesica@gmail.com>
2010-10-22 00:20:00 -0500

    Went back to html 5 doctype.

--- main.php
+++ main.php
@@ -17,8 +17,7 @@
 // must be run from within DokuWiki
 if (!defined('DOKU_INC')) die();
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!doctype html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
